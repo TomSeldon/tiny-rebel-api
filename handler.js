566b55cd085efe612407f0c38d6dd48e81fc5ca2
Tom Seldon <tom@tomseldon.co.uk>
2017-07-18 19:44:04 +0100

    [FEATURE] Add first pass at exposing drinks

--- handler.js
+++ handler.js
@@ -1,16 +1,31 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event
+const tinyRebelWebScraper = require('tiny-rebel-web-scraper');
+
+module.exports.getDrinks = (event, context, callback) => {
+    const barLocation = event.pathParameters.barLocation;
+
+    if (!barLocation) {
+        callback(new Error('[400] No bar location specified'));
+        return;
+    }
+
+    tinyRebelWebScraper
+        .getAllDrinks(barLocation)
+        .then(drinks => {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    drinks,
+                    total: drinks.length
+                })
+            };
+
+            callback(null, response);
         })
-    };
+        .catch(error => {
+            console.log(error);
 
-    callback(null, response);
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+            callback(new Error('[500] Unable to load drinks'));
+        });
 };
