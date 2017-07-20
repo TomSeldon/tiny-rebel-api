8a1f777dc07a825b1123f92a94816710c7678965
Tom Seldon <tom@tomseldon.co.uk>
2017-07-20 21:44:48 +0100

    [TEST] Alexa: Add tests
    Simple tests to invoke functions locally, and fail if any exit with a non-0 code.
    
    This won't catch everything, in fact very far from it. However, it's a nice safety net to check that everything isn't completely broken.

---
+++ alexa/test/test-local-invocation.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -u
+set -e
+
+# Run through each local invocation to check exit code is 0.
+# This doesn't guard against everything, but at least catches issues where
+# the function is broken.
+
+echo "\n\nTesting: Get cheapest drink"
+npm run invoke-local-get-cheapest
+npm run invoke-local-get-cheapest-no-bar
+
+echo "\n\nTesting: Get strongest drink"
+npm run invoke-local-get-strongest
+npm run invoke-local-get-strongest-no-bar
+
+echo "\n\nTesting: Suggest a drink"
+npm run invoke-local-suggest-drink
+npm run invoke-local-suggest-drink-no-bar
